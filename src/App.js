465c4e1589234f475e6096c85b7a6e6ad13053be
Victoriia <viktoria281294@gmail.com>
2017-12-11 15:12:54 +0200

    complete inputs: name, surname, date. Calculation age! Make size for inputs

--- src/App.js
+++ src/App.js
@@ -2,20 +2,47 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <input name="name" className="Inputs" id={"name"}/>
-                    <input name="surname" className="Inputs" id={"surname"}/>
+    constructor(props) {
+        super(props);
+        this.state = {name: '', surname: '', dateOfBirthday: '', age: ''};
 
-                </header>
-                <p className="App-intro">
-                    Hello!, dear {document.getElementById("name")}
-                    <h2>It is {new Date().toLocaleTimeString()}.</h2>
-                </p>
+        this.handleChange = this.handleChange.bind(this);
+        // this.getAge = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        if (event.target.name === 'name') {
+            this.setState({name: event.target.value});
+        } else if (event.target.name === 'surname') {
+            this.setState({surname: event.target.value});
+        } else if (event.target.name === 'birthday') {
+            this.setState({dateOfBirthday: event.target.value});
+        }
+    }
+
+    getAge() {
+        let now = (new Date()).getFullYear();
+        let year = this.state.dateOfBirthday.slice(0, 4);
+        return now - year;
+    }
+
+    render() {
+        return <div className="App">
+            <header className="App-header">
+                <input name="name" className="Inputs" value={this.state.name} onChange={this.handleChange}/>
+                <br/>
+                <input name="surname" className="Inputs" value={this.state.surname} onChange={this.handleChange}/>
+                <br/>
+                <input name="birthday" className="Inputs" type="date" value={this.state.dateOfBirthday}
+                       onChange={this.handleChange}/>
+
+            </header>
+            <div className="App-intro">
+                Hello!, dear {this.state.surname} {this.state.name}
+                <br/>
+                You age {this.getAge()}
             </div>
-        );
+        </div>;
     }
 }
 
